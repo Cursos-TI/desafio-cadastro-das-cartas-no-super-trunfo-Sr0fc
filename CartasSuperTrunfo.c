73a6c6e4120328a507fcf0056be645a38db44864
Sr0fc <168571916+Sr0fc@users.noreply.github.com>
2025-01-31 14:57:05 +0000

    Novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-    int main(){
+int main(){
 
-    
+    //Definição da estrutura para armazenar dados das cartas
+    char estado;
+    char codigocarta[2];
+    char cidade[50];
+    int populacao;
+    float area;
+    float pib;
+    int turisticos;
+
+
+
 }
